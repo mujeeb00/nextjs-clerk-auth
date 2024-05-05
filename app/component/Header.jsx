9e20744fc5dffb195a20bcf81383fca2e9c7bb2b
Mujeeb ur rehman <mujeebfx160@gmail.com>
2024-05-05 14:43:07 +0500

    Add : header

--- app/component/Header.jsx
+++ app/component/Header.jsx
@@ -2,13 +2,16 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Link from 'next/link';
-
+import { useAuth, UserButton } from '@clerk/nextjs';
 const Header = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     const toggleDrawer = () => {
         setIsDrawerOpen(!isDrawerOpen);
     };
+
+    const { userId } = useAuth();
+    console.log("id user ; ", userId);
 
     return (
         <nav className='bg-gradient-to-r from-gray-900 to-gray-800 py-4 lg:py-6'>
@@ -18,38 +21,52 @@ const Header = () => {
                         Clerk Auth
                     </div>
                 </Link>
-                <div className='lg:hidden'>
+                
+                     <div className='flex'>
+                     <div className='ms-4'>
+                         <UserButton afterSignOutUrl='/' />
+                     </div></div>
+               
+                {!userId&&(
+                    <div className='lg:hidden'>
                     {/* Hamburger Icon */}
                     <FaBars className='text-white cursor-pointer' onClick={toggleDrawer} />
                     {/* Drawer */}
                     {isDrawerOpen && (
                         <div className='fixed inset-0 z-50 bg-gray-800 bg-opacity-75'>
                             <div className='absolute inset-y-0 right-0 max-w-xs w-1/2 bg-white shadow-lg'>
-                                 {/* Close Drawer Button */}
-                                 <div className='p-4'>
-                                        <FaTimes className='text-gray-900 hover:text-gray-600 cursor-pointer' onClick={toggleDrawer} />
-                                    </div>
-                                    <div className='py-6 px-4 flex flex-col space-y-4'>
-                                    <Link href='/login'>
-                                        <div className='text-gray-900 hover:text-gray-600 text-lg font-semibold'>Login</div>
-                                    </Link>
-                                    <Link href='/signup'>
-                                        <div className='text-gray-900 hover:text-gray-600 text-lg font-semibold'>Signup</div>
-                                    </Link>
-                                    
+                                {/* Close Drawer Button */}
+                                <div className='p-4'>
+                                    <FaTimes className='text-gray-900 hover:text-gray-600 cursor-pointer' onClick={toggleDrawer} />
                                 </div>
+                                    <div className='py-6 px-4 flex flex-col space-y-4'>
+                                        <Link href='/sign-in'>
+                                            <div className='text-gray-900 hover:text-gray-600 text-lg font-semibold'>Login</div>
+                                        </Link>
+                                        <Link href='/sign-up'>
+                                            <div className='text-gray-900 hover:text-gray-600 text-lg font-semibold'>Signup</div>
+                                        </Link>
+                                    </div>
                             </div>
                         </div>
                     )}
                 </div>
-                <div className='hidden lg:flex space-x-6'>
-                    <Link href='/login'>
-                        <div className='text-white hover:text-gray-200 transition duration-300'>Login</div>
-                    </Link>
-                    <Link href='/signup'>
-                        <div className='text-white hover:text-gray-200 transition duration-300'>Signup</div>
-                    </Link>
-                </div>
+                )}
+                
+
+                {!userId && (
+                    <div className='hidden lg:flex space-x-6'>
+                        <Link href='/sign-in'>
+                            <div className='text-white hover:text-gray-200 transition duration-300'>Login</div>
+                        </Link>
+                        <Link href='/sign-up'>
+                            <div className='text-white hover:text-gray-200 transition duration-300'>Signup</div>
+                        </Link>
+                        
+                    </div>
+                )}
+
+
             </div>
         </nav>
     );
